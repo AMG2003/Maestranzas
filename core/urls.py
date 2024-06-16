8a4f81e456a19e1e3105f3ec50459d438a0483f4
JulioDiazRojo <ju.diazr@duocuc.cl>
2024-06-16 13:22:46 -0400

    Login y Registro

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home,base, exit, registro
 
 urlpatterns = [
-    path('', home, name="home")
+    path('', home, name="home"),
+    path('base/',base,name="base"),
+    path('logout/',exit,name="exit"),
+    path('registro/',registro,name="registro")
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import logout
+from django import forms
+from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request, "core/home.html")
+def base(request):
+    return render(request, "core/base.html")
+
+def exit(request):
+    logout(request)
+    return redirect('home')
+
+def registro(request):
+    data = {"form": CustomUserCreationForm()}
+
+    if request.method == "POST":
+        formulario = CustomUserCreationForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            user = authenticate(
+                username=formulario.cleaned_data["username"],
+                password=formulario.cleaned_data["password1"],
+            )
+            login(request, user)
+            messages.success(request, "Te has registrado correctamente")
+            return redirect(to="home")
+        data["form"] = formulario
+
+    return render(request, "registration/registro.html", data)
