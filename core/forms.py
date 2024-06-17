8dbca05a71886e2e11909c72bb09e0544dac41d7
JulioDiazRojo <ju.diazr@duocuc.cl>
2024-06-16 22:13:03 -0400

    Varios
    -Incorporación de perfil de usuario.
    -Listar piezas
    -Registrar Piezas

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Pieza
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username',"first_name","last_name","email","password1","password2"]
+
+
+class PiezaForm(forms.ModelForm):
+    class Meta:
+        model = Pieza
+        fields = ['descripcion','numero_serie','ubicacion', 'cantidad_disponible', 'cantidad_minima']
+        exclude = ['fecha_registro']
--- core/views.py
+++ core/views.py
@@ -4,6 +4,9 @@ from django import forms
 from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate,login
 from django.contrib import messages
+from .models import Pieza
+from .forms import PiezaForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request, "core/home.html")
@@ -31,3 +34,22 @@ def registro(request):
         data["form"] = formulario
 
     return render(request, "registration/registro.html", data)
+@login_required
+def perfil(request):
+    return render(request, "core/perfil.html")
+@login_required
+def registrar_pieza(request):
+    if request.method == 'POST':
+        form = PiezaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_piezas')
+        else:
+            return render(request, 'core/registrar_pieza.html', {'form': form})
+    else:
+        form = PiezaForm()
+    return render(request, 'core/registrar_pieza.html', {'form': form})
+@login_required
+def lista_piezas(request):
+    piezas = Pieza.objects.all()
+    return render(request, 'core/lista_piezas.html', {'piezas': piezas})
